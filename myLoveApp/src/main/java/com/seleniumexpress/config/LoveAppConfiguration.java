90556c41dc0259c426746ea96c13eb1b6150ccd8
HarikaPenukonda <penukondaharika@gmail.com>
2024-05-31 10:53:24 -0500

    creating LoveAppConfiguration

---
+++ myLoveApp/src/main/java/com/seleniumexpress/config/LoveAppConfiguration.java
@@ -0,0 +1,17 @@
+package com.seleniumexpress.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+public class LoveAppConfiguration implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		
+		
+	}
+
+}
