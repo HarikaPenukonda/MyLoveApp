ce9c86f2a685e9aa5382ac541a3ea446e84b05fa
HarikaPenukonda <penukondaharika@gmail.com>
2024-05-31 13:02:14 -0500

    sending jsp page instead of String

--- myLoveApp/src/main/java/com/seleniumexpress/controller/TestController.java
+++ myLoveApp/src/main/java/com/seleniumexpress/controller/TestController.java
@@ -2,15 +2,13 @@ package com.seleniumexpress.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 	
-	@ResponseBody
 	@RequestMapping("/hello")
 	public String sayHello() {
-		return "Hello...Welcome!!!"; 
+		return "hello"; 
 	}
 
 }
